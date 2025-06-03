42a0e37bbab87ed22a85d413cc543d16a174bf3a
unknown <huyen.le@infis.cz>
2025-06-03 11:02:15 +0200

    dhf

---
+++ test.py
@@ -0,0 +1,19 @@
+def pozdrav():
+    print("Ahoj, svete")
+
+def moje_jmeno():
+    print("HANA")
+
+def tri_radky():
+    print("To je skvele.")
+    print("Mam se dobre")
+    print("Chci zpatky do Rakouska")
+
+def zvuk_znovu():
+    for i in range(3):
+        print("DING")
+
+pozdrav()
+moje_jmeno()
+tri_radky()
+zvuk_znovu()
